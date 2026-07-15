wzming/sing-box

add rule-provider clash-api

route/rule_set_abstract.go
@@ -25,9 +25,11 @@ type abstractRuleSet struct {
 	router      adapter.Router
 	logger      logger.ContextLogger
 	tag         string
+	sType       string
 	path        string
 	format      string
 	rules       []adapter.HeadlessRule
+	ruleCount   uint64
 	metadata    adapter.RuleSetMetadata
 	lastUpdated time.Time
 	refs        atomic.Int32
@@ -35,6 +37,22 @@ type abstractRuleSet struct {
 
 func (s *abstractRuleSet) Name() string {
 	return s.tag
+}
+
+func (s *abstractRuleSet) Type() string {
+	return s.sType
+}
+
+func (s *abstractRuleSet) Format() string {
+	return s.format
+}
+
+func (s *abstractRuleSet) RuleCount() uint64 {
+	return s.ruleCount
+}
+
+func (s *abstractRuleSet) UpdatedTime() time.Time {
+	return s.lastUpdated
 }
 
 func (s *abstractRuleSet) String() string {
@@ -128,18 +146,21 @@ func (s *abstractRuleSet) loadBytes(content []byte) error {
 
 func (s *abstractRuleSet) reloadRules(headlessRules []option.HeadlessRule) error {
 	rules := make([]adapter.HeadlessRule, len(headlessRules))
-	var err error
+	var ruleCount uint64
 	for i, ruleOptions := range headlessRules {
-		rules[i], err = NewHeadlessRule(s.router, ruleOptions)
+		rule, err := NewHeadlessRule(s.router, ruleOptions)
 		if err != nil {
 			return E.Cause(err, "parse rule_set.rules.[", i, "]")
 		}
+		rules[i] = rule
+		ruleCount += rule.RuleCount()
 	}
 	var metadata adapter.RuleSetMetadata
 	metadata.ContainsProcessRule = hasHeadlessRule(headlessRules, isProcessHeadlessRule)
 	metadata.ContainsWIFIRule = hasHeadlessRule(headlessRules, isWIFIHeadlessRule)
 	metadata.ContainsIPCIDRRule = hasHeadlessRule(headlessRules, isIPCIDRHeadlessRule)
 	s.rules = rules
+	s.ruleCount = ruleCount
 	s.metadata = metadata
 	return nil
 }

luange/sing-box-luan-smart

route/rule/rule_set_abstract.go
@@ -27,9 +27,11 @@ type abstractRuleSet struct {
 	logger      logger.ContextLogger
 	tag         string
 	access      sync.RWMutex
+	sType       string
 	path        string
 	format      string
 	rules       []adapter.HeadlessRule
+	ruleCount   uint64
 	metadata    adapter.RuleSetMetadata
 	lastUpdated time.Time
 	callbacks   list.List[adapter.RuleSetUpdateCallback]
@@ -38,6 +40,22 @@ type abstractRuleSet struct {
 
 func (s *abstractRuleSet) Name() string {
 	return s.tag
+}
+
+func (s *abstractRuleSet) Type() string {
+	return s.sType
+}
+
+func (s *abstractRuleSet) Format() string {
+	return s.format
+}
+
+func (s *abstractRuleSet) RuleCount() uint64 {
+	return s.ruleCount
+}
+
+func (s *abstractRuleSet) UpdatedTime() time.Time {
+	return s.lastUpdated
 }
 
 func (s *abstractRuleSet) String() string {
@@ -113,11 +131,14 @@ func (s *abstractRuleSet) loadBytes(content []byte, ruleset adapter.RuleSet) err
 func (s *abstractRuleSet) reloadRules(headlessRules []option.HeadlessRule, ruleSet adapter.RuleSet) error {
 	rules := make([]adapter.HeadlessRule, len(headlessRules))
 	var err error
+	var ruleCount uint64
 	for i, ruleOptions := range headlessRules {
-		rules[i], err = NewHeadlessRule(s.ctx, ruleOptions)
+		rule, err := NewHeadlessRule(s.ctx, ruleOptions)
 		if err != nil {
 			return E.Cause(err, "parse rule_set.rules.[", i, "]")
 		}
+		rules[i] = rule
+		ruleCount += rule.RuleCount()
 	}
 	metadata := buildRuleSetMetadata(headlessRules)
 	err = validateRuleSetMetadataUpdate(s.ctx, s.tag, metadata)
@@ -126,6 +147,7 @@ func (s *abstractRuleSet) reloadRules(headlessRules []option.HeadlessRule, ruleS
 	}
 	s.access.Lock()
 	s.rules = rules
+	s.ruleCount = ruleCount
 	s.metadata = metadata
 	callbacks := s.callbacks.Array()
 	s.access.Unlock()